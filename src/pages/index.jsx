88979a7d58ebce6a6d5d6bff9e7ead7fb26d8a91
Josia Schweizer <josia.schweizer@gmail.com>
2025-01-23 17:32:42 +0100

    cleanup index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,30 +6,42 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>welcome | my portfolio</title>
-                <meta name="description" content="be inspired. welcome to my portfolio!" />
+                <title>Welcome | My Portfolio</title>
+                <meta
+                    name="description"
+                    content="Be inspired. Welcome to my portfolio!"
+                />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
 
             <main className={styles.entryContainer}>
                 <div className={styles.overlay}></div>
                 <h1 className={styles.title}>
-                    welcome, i am <span className={styles.highlight}>josia schweizer</span>
+                    Welcome, I am <span className={styles.highlight}>Josia Schweizer</span>
                 </h1>
                 <p className={styles.subtitle}>
-                    creator of <strong>unique works</strong>, photographer & videographer by passion.
+                    Creator of <strong>unique works</strong>, photographer & videographer
+                    by passion.
                 </p>
             </main>
 
             <section className={styles.gallery}>
-                <h2 className={styles.sectionTitle}>portfolio</h2>
+                <h2 className={styles.sectionTitle}>Portfolio</h2>
 
                 <div className={styles.grid}>
                     <div className={styles.gridItem}>
-                        <img src="/images/index-photo1.jpg" alt="image 1" className={styles.photo} />
+                        <img
+                            src="/images/index-photo1.jpg"
+                            alt="Image 1"
+                            className={styles.photo}
+                        />
                     </div>
                     <div className={styles.gridItem}>
-                        <img src="/images/index-photo2.jpg" alt="image 2" className={styles.photo} />
+                        <img
+                            src="/images/index-photo2.jpg"
+                            alt="Image 2"
+                            className={styles.photo}
+                        />
                     </div>
                 </div>
             </section>
